dd365808ac921a490c618376b227083271c1d10c
ausir <ausir@finpo.com.tw>
2017-07-10 10:36:16 +0800

    add example

---
+++ examples/base/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+<section>
+  <g-recaptcha 
+    data-sitekey="6LdTpxUUAAAAAG6L89kxRvjMdP0XDAyUji8rtQxw" 
+    :data-validate="validate"
+    :data-callback="callback"
+    :data-btn-class="'btn'"
+    :data-btn-disabled="true"
+     >
+    Submit form
+  </g-recaptcha>
+  <!-- g-recaptcha will replace a button for submit form -->
+</section>
+</template>
+<script>
+import gRecaptcha from '../../../../src/reCaptchaComponent.vue';
+export default {
+  components: {
+    gRecaptcha,
+  },
+  methods: {
+    validate() {
+      // validate your form , if you don't have validate prop , default validate pass .
+      return true;
+    },
+    callback(token) {
+      // google recaptcha token , then you can pass to backend with your form data .
+      if (token) {
+        alert(token);
+      }else{
+        // if you use data-size show reCAPTCHA , maybe you will get empty token.
+        alert('please check you are not robot');
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.btn {
+  border: 1px solid red;
+}
+</style>
+
